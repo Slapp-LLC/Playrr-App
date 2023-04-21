c35e94e6ace99fc86c9a0b1fd6a64601516371a2
Pierre <jpcampoverde9801@hotmail.com>
2023-04-21 17:26:47 -0300

    Added splash to chat

--- lib/screens/messages/components/chat_item.dart
+++ lib/screens/messages/components/chat_item.dart
@@ -14,62 +14,68 @@ class ChatItem extends StatefulWidget {
 class _ChatItemState extends State<ChatItem> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-            border: Border(
-                bottom:
-                    const BorderSide(width: 1, color: secondaryBackground))),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            AvatarImage(photoUrl: null),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Juanito Mendoza ',
-                          style: TextStyle(color: Colors.white, fontSize: 17),
-                        ),
-                        Text(
-                          'Date',
-                          style: TextStyle(color: bodyTextColor),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+          onTap: () {},
+          child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  border: Border(
+                      bottom: const BorderSide(
+                          width: 1, color: secondaryBackground))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AvatarImage(photoUrl: null),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Column(
                         children: [
-                          Text(
-                            'Message',
-                            style: TextStyle(
-                              color: bodyTextColor,
-                              fontSize: 17,
-                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Juanito Mendoza ',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17),
+                              ),
+                              Text(
+                                'Date',
+                                style: TextStyle(color: bodyTextColor),
+                              )
+                            ],
                           ),
-                          CircleAvatar(
-                            backgroundColor: greenPrimaryColor,
-                            radius: 10,
-                            child: Text(
-                              '1',
-                              style: TextStyle(color: Colors.black),
+                          Padding(
+                            padding: EdgeInsets.only(top: 5),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Message',
+                                  style: TextStyle(
+                                    color: bodyTextColor,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                                CircleAvatar(
+                                  backgroundColor: greenPrimaryColor,
+                                  radius: 10,
+                                  child: Text(
+                                    '1',
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                )
+                              ],
                             ),
                           )
                         ],
                       ),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        ));
+                    ),
+                  )
+                ],
+              ))),
+    );
   }
 }
